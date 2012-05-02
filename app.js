c9b90c7d70b38e11a9539b26654b1c06be4e7ee6
Denny Lim <hi.iamdenny@gmail.com>
2012-05-02 21:35:43 +0900

    socket.io

---
+++ app.js
@@ -0,0 +1,25 @@
+var io = require('socket.io').listen(2046);
+
+io.set('origins', '*:*')
+  .set('transports', [
+  	'websocket'
+  	, 'flashsocket'
+  	, 'xhr-polling'
+  	, 'htmlfile'
+  	, 'jsonp-polling'
+  ]);
+  
+  
+  
+var Login = io
+	.of('/login')
+	.on('connection', function(socket){
+		console.log('/login connection');
+	});
+
+
+var MyBook = io
+	.of('/book')
+	.on('connection', function(socket){
+		console.log('/book connection');
+	});
