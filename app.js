e654af836bcbca20217377c155144d71e9685d5e
Denny Lim <hi.iamdenny@gmail.com>
2013-03-15 11:52:33 +0900

    Update app.js
    test

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ var naverkey = '';
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  //res.end('Hello World\n');
+  console.log('start');
   
   http.get({
   	'hostname' : 'http://openapi.naver.com/search'
