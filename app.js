58de51297b6fc6836d4365128f191a6d325fe0f8
Denny Lim <hi.iamdenny@gmail.com>
2013-03-15 11:46:43 +0900

    Update app.js
    test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var io = require('socket.io').listen(2046);
 
 io.set('origins', '*:*')
@@ -23,3 +24,12 @@ var Book = io
 	.on('connection', function(socket){
 		console.log('/book connection');
 	});
+
+*/
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(2016, 'mybookmanagerserver.iamdenny.com');
+console.log('Server running at http://mybookmanagerserver.iamdenny.com:2046');
